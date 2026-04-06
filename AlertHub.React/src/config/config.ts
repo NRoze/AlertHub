61f3be7c49719b861588e1a8d92fd0b9831d5054
Nadav <nro008@gmail.com>
2026-04-06 21:08:13 +0300

    fix numeric env var

--- AlertHub.React/src/config/config.ts
+++ AlertHub.React/src/config/config.ts
@@ -2,6 +2,7 @@ const alertsSseUrl = import.meta.env.VITE_ALERTS_SSE_URL;
 if (!alertsSseUrl) throw new Error("Missing VITE_ALERTS_SSE_URL");
 
 const alertsTtlMs = Number(import.meta.env.VITE_ACTIVE_ALERT_TTL_MS);
+console.log("Config: ", { alertsSseUrl, alertsTtlMs });
 if (isNaN(alertsTtlMs)) {
   throw new Error("Invalid VITE_ACTIVE_ALERT_TTL_MS, must be a number");
 }
