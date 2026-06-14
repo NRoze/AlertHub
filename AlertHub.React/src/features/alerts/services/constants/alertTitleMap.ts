45f5e6a67a7531705c2d4d95af27307a1f722979
Nadav <nro008@gmail.com>
2026-06-14 19:19:59 +0300

    new alert titles

--- AlertHub.React/src/features/alerts/services/constants/alertTitleMap.ts
+++ AlertHub.React/src/features/alerts/services/constants/alertTitleMap.ts
@@ -3,11 +3,13 @@ import { AlertType } from "../../../shared/model/AlertType";
 export const titleToTypeMap = new Map<string, AlertType>([
   ["אין התרעות", AlertType.NO_ALERTS],
   ["חדירת כלי טיס עוין", AlertType.UAV_INTRUSION],
+  ["כלי טייס עוין - היכנסו מייד למרחב המוגן", AlertType.UAV_INTRUSION],
   ["ירי רקטות וטילים", AlertType.ROCKET_FIRE],
   ["ירי רקטות וטילים - היכנסו למרחב המוגן", AlertType.ROCKET_FIRE],
   ["חדירת מחבלים", AlertType.INFILTRATION],
   ["האירוע הסתיים", AlertType.EVENT_ENDED],
   ["בדקות הקרובות צפויות להתקבל התרעות באזורך", AlertType.PRE_ALERT],
+  ["איום מלבנון - התקרבו למרחב מוגן", AlertType.PRE_ALERT],
 ]);
 
 export const getTitleByType = (type: AlertType): string => {
